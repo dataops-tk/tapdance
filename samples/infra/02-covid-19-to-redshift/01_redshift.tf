050fe0c24d826915ad5dff2bafd3789dc981442f
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-05-10 15:41:47 -0700

    infra samples

---
+++ samples/infra/02-covid-19-to-redshift/01_redshift.tf
@@ -0,0 +1,26 @@
+# NOTE: Requires AWS policy 'AmazonRDSFullAccess' on the terraform account
+
+output "summary" { value = module.redshift.summary }
+module "redshift" {
+  source        = "git::https://github.com/slalom-ggp/dataops-infra.git//catalog/aws/redshift?ref=master"
+  name_prefix   = "${local.project_shortname}-"
+  environment   = module.env.environment
+  resource_tags = local.resource_tags
+
+  # CONFIGURE HERE:
+
+  identifier          = "rds-db"
+  admin_username      = "mysqladmin"
+  admin_password      = "asdfASDF12"
+  skip_final_snapshot = true # allows simple DB deletion for POC environments
+
+  /* OPTIONALLY, COPY-PASTE ADDITIONAL SETTINGS FROM BELOW:
+
+  mysql_version       = "5.7.26"
+  instance_class      = "db.t2.micro"
+  jdbc_port           = 3306
+  storage_size_in_gb  = 20
+
+  */
+
+}
