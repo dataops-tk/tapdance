51dc6be1af7fec1b3e9ea006430d43e7644f397d
AJ Steers <aj.steers@slalom.com>
2020-08-09 20:22:39 -0700

    adding back starting/ending log

--- tapdance/cli.py
+++ tapdance/cli.py
@@ -1,6 +1,7 @@
 """tapdance.cli - Defines the command-line interface (CLI) for tapdance."""
 
 import fire
+from logless import logged
 
 from tapdance.config import print_version
 from tapdance.docker import build_all_images, build_image
@@ -9,6 +10,7 @@ from tapdance.plans import plan
 from tapdance.syncs import sync
 
 
+@logged("tapdance execution")
 def main():
     """Run the tapdance CLI."""
     fire.Fire(
